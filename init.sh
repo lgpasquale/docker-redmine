442670af67bccfd4bcac5e5d58d388ba320c7250
Luca Pasquale <luca.pasquale@moxoff.com>
2016-01-11 11:50:38 +0100

    Environment variables should now be correctly used to set the name of the apache virtual host

--- init.sh
+++ init.sh
@@ -11,9 +11,11 @@ sed -i "s/\([[:space:]]*port:\).*/\1 ${MYSQL_PORT_3306_TCP_PORT}/g" /etc/redmine
 
 if [ -z "${REDMINE_SERVER_NAME}" ]; then
     sed -i "s/\([[:space:]]*ServerName\).*/\1 ${REDMINE_SERVER_NAME}/g" /etc/apache2/sites-available/redmine.conf
+    sed -i "s/\([[:space:]]*ServerName\).*/\1 ${REDMINE_SERVER_NAME}/g" /etc/apache2/sites-available/redmine-ssl.conf
 fi
 
 if [ -z "${REDMINE_SERVER_ALIAS}" ]; then
+    sed -i "s/\([[:space:]]*ServerAlias\).*/\1 ${REDMINE_SERVER_ALIAS}/g" /etc/apache2/sites-available/redmine.conf
     sed -i "s/\([[:space:]]*ServerAlias\).*/\1 ${REDMINE_SERVER_ALIAS}/g" /etc/apache2/sites-available/redmine-ssl.conf
 fi
 
